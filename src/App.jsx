11a3587eea24e93068823176468aa31d50bf5e37
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-07-19 00:17:52 +0530

    tawilwind css added, react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
 	const [count, setCount] = useState(0)
 
 	return(
-		<div>hello chatify project</div>
+		<div className='text-center'>hello chatify project</div>
 	)
 }
 
