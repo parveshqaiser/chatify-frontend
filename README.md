0ae67e7791a85a736b1acf472b333a7ed89aeb5f
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-08-25 20:31:52 +0530

    added typing feature

--- README.md
+++ README.md
@@ -14,13 +14,16 @@ pending :
     rtk interceptor, 
     get all online users,           done
     scrollview of chat smooths,     done
-    load 20 messages at once,
+    load 20 messages at once,       issues exist
     chat bubble UI,                 done
     message seen or not,
     use of double tick ,
     logout modularity ,             done
     group by date,                  done (some bus is there)
     try mongodb grouping            
+    message typing      
+
+    delete account
 
 
 
--- src/utils/socket-client.js
+++ src/utils/socket-client.js
@@ -3,6 +3,24 @@ import { io } from "socket.io-client";
 
 let BASE_URL = "http://localhost:7500";
 
-export const socketConnection = ()=>{
-    return io(BASE_URL)
-}
+// export const socketConnection = ()=>{
+//     return io(BASE_URL)
+// }
+
+
+let socket = null;
+
+export let createSocketConnection = () => {
+    if (!socket) {
+        socket = io(BASE_URL);
+    }
+
+    return socket;
+};
+
+export let disconnectSocket = () => {
+    if (socket) {
+        socket.disconnect();
+        socket = null;
+    }
+};
